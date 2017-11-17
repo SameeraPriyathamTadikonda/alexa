3cff0eebfc9899acbb076d9e50f764b1a5ef3e5b
Abhi <you@example.com>
2017-11-17 00:40:12 -0800

    initial

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@
 /*
  * GET home page.
  */
-var mysql = require('../mysql');
-var queryToChangeBackground = "UPDATE `alexadb`.`config_params` SET `value`='2' WHERE `key`='background_image'";
+var mysql = require('./mysql');
+var queryToChangeBackground = "UPDATE `alexadb`.`config_params` SET `value`='4' WHERE `key`='background_image'";
 
 exports.index = function(req, res){
 	mysql.fetchData(
